2b5f02dcb779faec7d731e34c7d20cbb4010a155
ruskaof <199-41@mail.ru>
2024-11-19 13:18:56 +0300

    fix: make fallback methods better

--- vbet-bets-handler/src/main/kotlin/ru/itmo/bets/handler/client/SportsClient.kt
+++ vbet-bets-handler/src/main/kotlin/ru/itmo/bets/handler/client/SportsClient.kt
@@ -1,12 +1,14 @@
 package ru.itmo.bets.handler.client
 
-import jakarta.servlet.UnavailableException
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import ru.itmo.common.response.MatchResponse
+import ru.itmo.common.response.SportResponse
+import java.util.logging.Level
+import java.util.logging.Logger
 
 
 @FeignClient(name = "sports-client", fallback = Fallback::class)
@@ -21,10 +23,11 @@ interface SportsClient {
 @Component
 class Fallback : SportsClient {
     override fun getMatch(id: Long): MatchResponse {
-        throw UnavailableException("Sports is currently unavailable")
+        Logger.getLogger("sports-client").log(Level.WARNING) { "Using fallback method on get match" }
+        return MatchResponse(-1, "Stub match", SportResponse(-1, "Stub sport"), false)
     }
 
     override fun endMatch(id: Long) {
-        throw UnavailableException("Sports is currently unavailable")
+        Logger.getLogger("sports-client").log(Level.WARNING) { "Using fallback method on end match" }
     }
 }
